d80c37ae1c4cf413564e1131db26a673dae371af
lfelipev <felipemmha@gmail.com>
2022-11-16 16:11:46 -0300

    Figuras stephens

---
+++ Figuras/stephens_fs.m
@@ -0,0 +1,12 @@
+P = 0:0.1:20;
+
+Qlvad = (10.59 + (-10.59./(1+(P./5.43).^2.79)));
+Qrvad = (10.59 + (-10.59./(1+(P./3.94).^2.27)));
+SF = 1;
+plot(P,Qlvad*SF,P,Qrvad*SF, 'linewidth',2)
+legend('QLVAD', 'QRVAD', 'Location','southeast');
+grid on
+xlabel('Tempo (s)')
+ylabel('Fluxo (L/min)','interpreter','latex')
+set(gca,'FontSize',16)
+set(gca,'fontname','times')
